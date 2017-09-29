7d7d12949d6bfa377772a786d2a9de8a5d59d268
Ness Bokobza <bokoness@gmail.com>
2017-09-29 10:17:02 +0300

    mission1

---
+++ pyhon/mission1.py
@@ -0,0 +1,74 @@
+#####################################
+#### PART 6: EXERCISE REVIEW  #######
+#####################################
+
+# Time to review all the basic data types we learned! This should be a
+# relatively straight-forward and quick assignment.
+
+###############
+## Problem 1 ##
+###############
+
+# Given the string:
+s = 'django'
+
+# Use indexing to print out the following:
+# 'd'
+
+# 'o'
+
+# 'djan'
+
+# 'jan'
+
+# 'go'
+
+# Bonus: Use indexing to reverse the string
+
+
+###############
+## Problem 2 ##
+###############
+
+# Given this nested list:
+l = [3,7,[1,4,'hello']]
+# Reassign "hello" to be "goodbye"
+l1 = l[2][2] = 'goodbye'
+print(l1)
+
+###############
+## Problem 3 ##
+###############
+
+# Using keys and indexing, grab the 'hello' from the following dictionaries:
+
+d1 = {'simple_key':'hello'}
+print(d1['simple_key'])
+
+d2 = {'k1':{'k2':'hello'}}
+print(d2['k1']['k2'])
+
+d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+print( d3['k1'][0]['nest_key'][1][0] )
+
+###############
+## Problem 4 ##
+###############
+
+# Use a set to find the unique values of the list below:
+mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
+mylist1 = set(mylist)
+print(mylist1)
+
+###############
+## Problem 5 ##
+###############
+
+# You are given two variables:
+age = 4
+name = "Sammy"
+
+# Use print formatting to print the following string:
+"Hello my dog's name is Sammy and he is 4 years old"
+
+print("Hello my dog's name is " + name + " and he is " +  str(age) + " years old")
