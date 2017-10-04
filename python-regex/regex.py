04340579a615704b16a3d18365b6d3db03ec81ef
Ness Bokobza <Bokoness@users.noreply.github.com>
2017-10-04 18:01:54 +0300

    regex

--- python-regex/regex.py
+++ python-regex/regex.py
@@ -17,3 +17,39 @@ print (match.start()) # the index of the match in string
 text = "Ness, im Ness, my name is Ness, and its a Ness"
     #params: 1. pattern, 2. text 
 print(re.findall('Ness', text))
+
+print('\n')
+
+def multi_re_find(patterns, phrase):
+    
+    for pat in patterns: 
+        print('Searching for pattern {}'.format(pat))
+        print(re.findall(pat, phrase))
+        print('\n')
+        
+
+test_phrase = 'sdsd..sssddd..sdddsddd...dsds...dssssss...sddddd'
+
+# *
+test_patterns = ['sd*'] # * - find letter 'd' 0 or more 'd'
+multi_re_find(test_patterns, test_phrase)
+
+# +
+test_patterns = ['sd+'] # + - find letter 'd' 1 or more
+multi_re_find(test_patterns, test_phrase)
+
+# ?
+test_patterns = ['sd?'] # + - find letter 'd' 0 or 1 time
+multi_re_find(test_patterns, test_phrase)
+
+# {x} count - pattern with x number of items 
+test_patterns = ['sd{3}'] # + - find letter 'd' 3 times
+multi_re_find(test_patterns, test_phrase)
+
+# {x, y} count - pattern with x or y number of items 
+test_patterns = ['sd{1,3}'] # + - find letter 'd' 1 or 3 times
+multi_re_find(test_patterns, test_phrase)
+
+# [x,y] 
+test_patterns = ['s[sd]+'] # + - find letter 's' followed with 1 or more number of s, OR followed by 1 or more number of d
+multi_re_find(test_patterns, test_phrase)
